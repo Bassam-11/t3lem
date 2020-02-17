774f7b1a41e9f1b3d9eec53bf11cd7e0586db017
unknown <bsam-1414@hotmail.com>
2020-02-17 22:07:40 +0300

    Added Files

---
+++ program_3/absher.py
@@ -0,0 +1,12 @@
+absher = [] 
+first_name = input (" your first name : ")  
+last_name = input (" your last name : ") 
+middle_name = input (" your middle name : ") 
+id_number = input (" your id number : ") 
+e_mail = input (" your e-mail : ") 
+
+print (" your first name is ", first_name ) 
+print (" your last name is ", last_name ) 
+print (" your middle name is ", middle_name ) 
+print (" your id number is ", id_number ) 
+print (" your e-mail is ", e_mail ) 
---
+++ program_4/car_info.py
@@ -0,0 +1,12 @@
+car_info = [] 
+car_type = input (" your car type : ") 
+car_model = input (" your car model : ")	
+id_numper = input (" your id numper : ") 
+mobile_numper = input (" your mobile numper : ") 
+license_numper = input (" your license numper : ") 
+
+print (" your car type is " , car_type ) 
+print (" your car model is " , car_model ) 
+print (" your id numper is " , id_numper )  
+print (" your mobile numper is " , mobile_numper )
+print (" your license numper is " , license_numper ) 
