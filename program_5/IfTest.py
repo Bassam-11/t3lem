5c463c9d61915d3061f916db36fbd14fd5393cd0
unknown <bsam-1414@hotmail.com>
2020-02-21 17:12:10 +0300

    add files

---
+++ program_5/IfTest.py
@@ -0,0 +1,9 @@
+first_name = input (" your first name : ")
+
+if first_name == ("Mohammed"):
+	print ("your first name is", first_name ) 
+elif first_name == ("Bassam"):
+	print (" your first_name is ", first_name)
+	
+else:
+	print(" your first name is wrong " ) 
---
+++ program_6/car_type.py
@@ -0,0 +1,3 @@
+car_type = input (" your car type : ") 
+if car_type == (" sonata"): 
+ print (" your car is cool " , car_type )               
