64e293128cd348bd7a32b176465e7362b567119e
unknown <bsam-1414@hotmail.com>
2020-02-21 19:12:14 +0300

    add files

--- program_6/car_type.py
+++ program_6/car_type.py
@@ -1,10 +1,33 @@
-car_type = input (" your car type : ") 
+car_tybe = input ("your car type :")
+bool = "No"
+if bool == ("No"):
+	print ("put your car type agian")
+	car_tybe = input ("your car type :")
+	
+if car_tybe == ("sonata"):
+	print ("good car") 
+	print (" the spare parts not so expensive") 
+	bool = "yes"
+if car_tybe == ("acoord") or ("elantra") or ("accent"):
+	print ("good car") 
+	print (" the spare parts not so expensive") 
+	bool = "yes"
+if car_tybe == ("doge"):
+	print ("cool car")
+	print	("but the spare parts so expensive")
+	bool = "yes"
+if car_tybe == ("mazda"):
+	print ("bad car")
+	print	(" but i dont like it ")
+	bool = "yes"
+if car_tybe == ("camaro"):
+	print (" very cool car") 
+	print ("but the spare parts so expensive ")
+	bool = "yes"
+if	car_tybe == ("ford"):
+	print ("god be with you") 	
+	print (" the spare parts so expensive")
+	bool = "yes"	
+else:
+	bool = "No"
 
-if car_type == ("sonata"): 
-	print (" your car is cool " , car_type )
-if car_type == (" sonata"): #Hello Bassam Please Update this one from down on: 
-	print (" your car is cool " , car_type )
-if car_type == (" sonata"):
-	print (" your car is cool " , car_type )
-if car_type == (" sonata"):
-	print (" your car is cool " , car_type )
