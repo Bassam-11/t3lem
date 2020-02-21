4f04f2ed56cebe7d977aadbc09ef1f5baf766ada
balswyan <balswyan@github.com>
2020-02-21 17:20:24 +0300

    Updated Program 6 with comment

--- program_6/car_type.py
+++ program_6/car_type.py
@@ -1,3 +1,10 @@
 car_type = input (" your car type : ") 
-if car_type == (" sonata"): 
- print (" your car is cool " , car_type )               
+
+if car_type == ("sonata"): 
+	print (" your car is cool " , car_type )
+if car_type == (" sonata"): #Hello Bassam Please Update this one from down on: 
+	print (" your car is cool " , car_type )
+if car_type == (" sonata"):
+	print (" your car is cool " , car_type )
+if car_type == (" sonata"):
+	print (" your car is cool " , car_type )
