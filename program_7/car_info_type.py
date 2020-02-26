125cb44d85912626fbcd0ce00b23a8085d1fd4f6
unknown <bsam-1414@hotmail.com>
2020-02-26 22:15:31 +0300

    add files

---
+++ program_7/car_info_type.py
@@ -0,0 +1,20 @@
+right = ("false") 
+i = 0 
+car_info = input ("your car company is :") 
+car_type = input ("your car type is :") 
+if car_info == ("honday") :
+	print ("your car type is :")  
+		if car_type == ("sonata"): 
+	print ("good car")
+	print ("the spare parts not so expensive")
+		if car_type == ("acoord") or ("elintra") or ("acsint") :
+	print ("good car")
+	print ("the spare parts not so expensive") 
+if car_info = ("doge"): 
+	print ("your car type is :")
+if car_type == ("charger") or ("challenger") or ("ram")	: 
+	print	("cool car") 
+	print ("but the spare parts so expensive") 
+if car_info == ("lexus"):
+	print ("your car type is : ")
+	
---
+++ program_7/car_info_type1.py
@@ -0,0 +1,59 @@
+right = ("false") 
+i = 0 
+while right == ("false"): 
+	car_type = input (" your car type is ")
+	if car_type == ("sonata") or ("acoord") or ("elntra"):
+		print (" your car is good ")
+		print (" the spaer parts not so expensive")
+		right = "true"
+	else:
+		print ("Try Agian Please")
+		right = "false"
+		i = i + 1
+	
+	if right == "true":
+		print ("thx for using the My program")
+		print (i)
+while right == ("false"):
+	car_type = input (" your car type is : ")
+	if car_type ==("doge") or ("challenger"):
+		print (" your car is cool "  )
+		print (" but the spaer parts so expensive")
+		right = "true"
+	else:
+		print ("Try Agian Please")
+		right = "false"
+		i = i + 1
+	
+	if right == "true":
+		print ("thx for using the My program")
+		print (i)
+while right == ("false"):
+	car_type = input (" your car type is : ")
+	if car_type ==("camrey") or ("orion") or ("hilux") :	
+		print ("your car is so good " )
+		print ("the spaer parts is not expensive")
+		right = "true"
+	else:
+		print ("Try Agian Please")
+		right = "false"
+		i = i + 1
+	
+	if right == "true":
+		print ("thx for using the My program")
+		print (i)
+while right == ("false"):
+	car_type = input (" your car type is : ")
+	if car_type ==("Ford") or ("toreis") :	
+		print ("your car is bad" )
+		print ("the spaer parts is so expensive")
+		print (" God be whit you ")
+		right = "true"
+	else:
+		print ("Try Agian Please")
+		right = "false"
+		i = i + 1
+	
+	if right == "true":
+		print ("thx for using the My program")
+		print (i)
